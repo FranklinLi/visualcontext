382453f5f80a1583bc2550ebbc4de50d1f0d2b7e
FranklinLi <FranklinLi@users.noreply.github.com>
2018-11-17 14:13:48 -0500

    Code works great. Remember give the permission to APP of storage use from the phone. I used Pixel 2 as the device layout.
    PreferenceActivity.java: used checkbox for input the restriction(The name of Preference Activity actually aims for restriction, just the name difference. After user input the checkbox, then click the button submit. then the system would output the restrictions to a file called restriction.txt)
    
    RestrictionActivity.java: click the type of food (pork, beef, chicken, seafood). Then it would go to PreferencePageActivity.java
    
    PreferencePageActivity.java: Used Likert scale (like strongly - dislike strongly). Click the scale and the function would output a file called pork.txt, beef.txt, chicken.txt, seafood.txt. The file name depends on the previous click. Then it would save the preference into the file.

--- app/src/main/java/com/example/franklin/visualcontext/PreferenceActivity.java
+++ app/src/main/java/com/example/franklin/visualcontext/PreferenceActivity.java
@@ -3,13 +3,87 @@ package com.example.franklin.visualcontext;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
 
-public class PreferenceActivity extends AppCompatActivity {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
 
+public class PreferenceActivity extends AppCompatActivity implements View.OnClickListener{
+
+    File root = android.os.Environment.getExternalStorageDirectory();
+    File dir = new File (root.getAbsolutePath());
+    File file;
+    CheckBox peanutallergies,halal,glutenfree,vegetarian,kosher;
+    String message = "";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_preference);
         Intent intent = getIntent();
+
+        peanutallergies=(CheckBox)findViewById(R.id.PeanutAllergies);
+        halal=(CheckBox)findViewById(R.id.Halal);
+        glutenfree=(CheckBox)findViewById(R.id.GlutenFree);
+        vegetarian=(CheckBox)findViewById(R.id.Vegetarian);
+        kosher=(CheckBox)findViewById(R.id.Kosher);
+
+        Button one = (Button) findViewById(R.id.button7);
+        one.setOnClickListener(this);
     }
+
+    public void onClick(View view){
+
+        switch (view.getId()) {
+
+            case R.id.button7:
+
+                if(peanutallergies.isChecked()){
+                    message = message + "peanutallergies,";
+                }
+                if(halal.isChecked()){
+                    message = message + "halal,";
+                }
+                if(kosher.isChecked()){
+                    message = message + "kosher,";
+                }
+                if(vegetarian.isChecked()){
+                    message = message + "vegetarian,";
+                }
+                if(glutenfree.isChecked()){
+                    message = message + "glutenfree";
+                }
+                file = new File(dir, "restriction.txt");
+                try {
+                    FileOutputStream f = new FileOutputStream(file);
+                    PrintWriter pw = new PrintWriter(f);
+                    pw.println(message);
+                    pw.flush();
+                    pw.close();
+                    f.close();
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                message = "";
+                break;
+            default:
+                break;
+        }
+
+
+
+
+    }
+
+
+
+
 }
