d21f4e3375ec5533ffd1a1a10f5d2a2bb2cf0564
FranklinLi <FranklinLi@users.noreply.github.com>
2018-11-17 00:31:22 -0500

    Added Restriction and Preference

---
+++ app/src/main/java/com/example/franklin/visualcontext/RestrictionActivity.java
@@ -0,0 +1,16 @@
+package com.example.franklin.visualcontext;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RestrictionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_restriction);
+        Intent intent = getIntent();
+        
+    }
+}
